14aa2910a96fd715c44ce528ad33ae8baa336349
chamuditha01 <heshanchamuditha05@gmail.com>
2024-10-18 08:45:02 +0530

    update. logger

--- logger.php
+++ logger.php
@@ -1,5 +1,6 @@
 <?php
 function writeLog($message) {
+    date_default_timezone_set('Asia/Colombo'); // Set the timezone to Sri Lanka
     $logFile = 'log.txt'; // Change to your desired log file path
     $timestamp = date("Y-m-d H:i:s");
     $logMessage = "[$timestamp] $message" . PHP_EOL;
